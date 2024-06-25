0d1ce6e4486bbd7ef592c51c80436d7cd9e33e86
rayfiyo <rayfiyorex@gmail.com>
2024-06-25 15:00:57 +0900

    del some line

--- server/server.go
+++ server/server.go
@@ -6,8 +6,6 @@ import (
 )
 
 func main() {
-	port := "8080"
-	log.Printf("listen on http://localhost:%s", port)
 	http.Handle("/", http.FileServer(http.Dir("public")))
-	http.ListenAndServe(":"+port, nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
